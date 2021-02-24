284cd8248e6aada640b7420f9a2b650a28789fdc
Ziya Mukhtarov <ziya.mukhtarov@ug.bilkent.edu.tr>
2021-02-24 11:38:10 +0400

    Initial Project Creation

---
+++ backend/src/main/java/tr/com/bilkent/fods/FoodOrderingAndDeliverySystemApplication.java
@@ -0,0 +1,11 @@
+package tr.com.bilkent.fods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingAndDeliverySystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingAndDeliverySystemApplication.class, args);
+	}
+}
